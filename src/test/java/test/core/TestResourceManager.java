a95ef63124311bd4d351d26aca3cd926294a30b9
ggstar <slatmother@gmail.com>
2013-08-16 15:17:15 +0400

    копирайты

--- src/test/java/test/core/TestResourceManager.java
+++ src/test/java/test/core/TestResourceManager.java
@@ -1,13 +1,3 @@
-/*
-* $Id
-*
-* (C) Copyright 1997 i-Teco, CJSK. All Rights reserved.
-* i-Teco PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
-*
-* Эксклюзивные права 1997 i-Teco, ЗАО.
-* Данные исходные коды не могут использоваться и быть изменены
-* без официального разрешения компании i-Teco.          
-*/
 package test.core;
 
 import java.io.IOException;
